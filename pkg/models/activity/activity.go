1ccb87c48839f3d3e93f191e4886eab7a50a4e97
wu-json <jason.wu2153@gmail.com>
2023-07-07 17:50:22 -0400

    feat: wipe orm

--- pkg/models/activity/activity.go
+++
@@ -1,8 +0,0 @@
-package activity
-
-type Activity struct {
-	Id           int64
-	Type         string
-	Summary      string
-	ValueDollars int64
-}
--- pkg/models/main.go
+++ pkg/models/main.go
@@ -1,12 +1,4 @@
 package main
 
-import (
-	"github.com/go-pg/pg/v10"
-)
-
 func main() {
-	db := pg.Connect(&pg.Options{
-		User: "postgres",
-	})
-	defer db.Close()
 }
