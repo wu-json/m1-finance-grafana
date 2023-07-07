6dfc794f584c608e623727bb83195578d055f900
wu-json <jason.wu2153@gmail.com>
2023-07-07 19:49:22 -0400

    feat: migrations

--- database/migrations/1_initialize_dividends_schema.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dividends (
-    id SERIAL PRIMARY KEY NOT NULL,
-    ticker VARCHAR NOT NULL,
-    dollar_value MONEY,
-    received_on TIMESTAMP NOT NULL UNIQUE
-);
-
-CREATE INDEX ix_dividends_ticker ON dividends(ticker);
-CREATE INDEX ix_dividends_date ON dividends(date);
