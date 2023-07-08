555d6e288e6e6ad19c89d2b167ede64256107e90
wu-json <jason.wu2153@gmail.com>
2023-07-07 22:12:00 -0400

    feat: example insert

--- parse-dividends/main.go
+++ parse-dividends/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/wu-json/m1-finance-grafana/parse-dividends/sqlc"
+)
+
+func run() error {
+	ctx := context.Background()
+
+	db, err := sql.Open("postgres", "user=user pass=pass dbname=m1finance sslmode=disable")
+	if err != nil {
+		return err
+	}
+
+	queries := sqlc.New(db)
+
+	err = queries.CreateDividends(ctx, sqlc.CreateDividendsParams{
+		Ticker:      "VOO",
+		DollarValue: sql.NullString{String: "100.00"},
+		ReceivedOn:  time.Now(),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("hello world")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
