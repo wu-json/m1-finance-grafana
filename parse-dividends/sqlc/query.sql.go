b6cafd7438b9bc2b5b5d196f35e0ce4e7e96e01a
wu-json <jason.wu2153@gmail.com>
2023-07-07 22:55:55 -0400

    fix: on conflict

--- parse-dividends/sqlc/query.sql.go
+++ parse-dividends/sqlc/query.sql.go
@@ -12,7 +12,7 @@ import (
 )
 
 const createDividends = `-- name: CreateDividends :exec
-INSERT INTO dividends (ticker, dollar_value, activity_type, received_on) VALUES ($1, $2, $3, $4) ON CONFLICT (received_on) DO NOTHING
+INSERT INTO dividends (ticker, dollar_value, activity_type, received_on) VALUES ($1, $2, $3, $4) ON CONFLICT (ticker, activity_type, received_on) DO NOTHING
 `
 
 type CreateDividendsParams struct {
